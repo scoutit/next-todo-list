95ba16d5706c989e33e67424bd4f07e659404fbf
scoutit <erniedl.00@gmail.com>
2025-10-30 12:33:21 -0500

    wording updates in todo list

--- src/app/components/TodoHeader.tsx
+++ src/app/components/TodoHeader.tsx
@@ -6,18 +6,24 @@ type TodoHeaderProps = {
   addTodo: () => void;
 };
 
-export default function TodoHeader({ input, setInput, addTodo }: TodoHeaderProps) {
+export default function TodoHeader({
+  input,
+  setInput,
+  addTodo,
+}: TodoHeaderProps) {
   return (
     <>
-      <h1 className={"title-header"}>Jobs for which my ass needs to apply</h1>
+      <h1 className={"title-header"}>Jobs To-Do</h1>
       <div style={{ display: "flex", gap: 8, marginBottom: 16 }}>
         <input
           value={input}
-          onChange={e => setInput(e.target.value)}
-          placeholder="Add a new task"
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Add a new company to your to-do list"
           style={{ flex: 1, padding: 8 }}
         />
-        <button className={"bg-blue-500 add-remove-button"} onClick={addTodo}>Add</button>
+        <button className={"bg-blue-500 add-remove-button"} onClick={addTodo}>
+          Add
+        </button>
       </div>
     </>
   );
